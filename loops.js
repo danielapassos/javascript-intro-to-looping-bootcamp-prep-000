d527f9c6efa529649817dd44ec644f6021a376f4
Daniela Passos <moitidani@gmail.com>
2018-07-05 23:20:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,8 @@
 function forLoop(array) {
-  
   for (var i=0; i<25; i++){
     if (i==1)
-      console.log("I am "+ i + " strange loop.");
+      array.push("I am "+ i + " strange loop.");
     else
-      console.log("I am "+ i + " strange loops.");
+      array.push("I am "+ i + " strange loops.");
   }
 }
